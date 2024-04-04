b8e728a768fcc6334ebd8762fc7f64365aaa8708
Usaid Fahad <usaidfahad9@gmail.com>
2024-04-05 03:16:34 +0530

    completed new project

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import InputBox from './InputBox'
+
+export { InputBox }
+//multiple files can be imported here form many places and exported together from one file so that it is easier to import them in further files
